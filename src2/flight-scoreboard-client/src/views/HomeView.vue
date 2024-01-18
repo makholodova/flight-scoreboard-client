dfdc5f9898fc0480e64869f5f80aad9deaeded2f
Marina Kholodova <marinaholodova@gmail.com>
2024-01-18 20:59:41 +0300

    axios

--- src2/flight-scoreboard-client/src/views/HomeView.vue
+++ src2/flight-scoreboard-client/src/views/HomeView.vue
@@ -1,5 +1,11 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+//import axios from 'axios'
+
+/*axios
+  .get('https://localhost:7294/City')
+  .then(response => (props.cities = response.data));*/
+
 </script>
 
 <template>
